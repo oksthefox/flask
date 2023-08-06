30bcc13c69cdb03d40b05cdd666e3e93e7daaae4
oksthefox <sh2oxana@gmail.com>
2023-08-06 15:09:14 +0300

    p

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, send_from_directory, request, Response
 import mysql.connector
 import os
 import random
 import time
+from flask import Flask, render_template, send_from_directory
+from prometheus_client import Counter
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
-app = Flask(__name__, template_folder='Web', static_folder='CSS')
+
+app = Flask(__name__, template_folder='Web', static_folder='Static')
+
+CV_downloads = Counter('CV_downloads', 'Number of times CV has been downloaded')
 
 # Configure MySQL connection
 db = None
@@ -20,13 +27,28 @@ while db is None:
         print("Failed connecting to database. Retrying...")
         time.sleep(1)
 
-@app.route("/")
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def test():
+    if request.method == "POST":
+        CV_downloads.inc()
+    return render_template("test.html")
+
+
+@app.route("/gifs", methods=["GET", "POST"])
 def index():
     cursor = db.cursor()
     cursor.execute("SELECT url FROM images;")
     images = [row[0] for row in cursor.fetchall()]
     url = random.choice(images)
     return render_template("index.html", url=url)
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == "__main__":
